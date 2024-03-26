878a536f8a63a1e59152929473fb6f22a69d3084
gunjanmhjn <gm4770@gmail.com>
2024-03-27 00:51:33 +0530

    first commit

---
+++ project.py
@@ -0,0 +1,11 @@
+#import numpy as np
+
+import pandas as pd
+print(pd.__version__)
+# import matplot.lib.pyplot as plt
+# import yfinance as yf
+# start = '2012-01-01'
+# end = '2022-12-21'
+# stock = 'GOOG'
+# I
+# data = yf.download (stock, start, end)
